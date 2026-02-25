df973ba280c23ec57d1407979d1ef61bc7478ffe
Bret Bouchard <bret@example.com>
2026-02-25 00:57:06 -0500

    feat(01-01): add GSD_Core module implementation
    - Add GSD_Core.cpp with FGSD_CoreModule class
    - Log startup and shutdown messages using GSD_LOG macro
    - Verify plugin loads correctly in Unreal Editor

---
+++ Plugins/GSD_Core/Source/GSD_Core/Private/GSD_Core.cpp
@@ -0,0 +1,23 @@
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+#include "GSDLog.h"
+
+#define LOCTEXT_NAMESPACE "FGSD_CoreModule"
+
+class FGSD_CoreModule : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override
+    {
+        GSD_LOG(Log, "GSD_Core module starting up");
+    }
+
+    virtual void ShutdownModule() override
+    {
+        GSD_LOG(Log, "GSD_Core module shutting down");
+    }
+};
+
+IMPLEMENT_MODULE(FGSD_CoreModule, GSD_Core)
+
+#undef LOCTEXT_NAMESPACE
